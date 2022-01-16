419cbfce07c9bbcfeb4ec5fd6594637f0fdc84dc
Rong Mantle Bao <baorong2005@126.com>
2022-01-16 10:07:49 +0800

    perf(model): abandon costly WASM interop

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 const path = require("path")
-const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin")
 
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
-    plugins: [
-      new WasmPackPlugin({
-        crateDirectory: path.resolve(__dirname, "."),
-        // pluginLogLevel: "error"
-      }),
-    ],
-    experiments: {
-      asyncWebAssembly: true,
-    },
+    plugins: [],
+    experiments: {},
   })
 }
