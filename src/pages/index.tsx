e638d6a8c10c7f7b33014dba2bd8d2ed94358ad4
Rong Mantle Bao <baorong2005@126.com>
2022-02-06 12:59:13 +0800

    feat(ui): finish index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,84 @@
+import { graphql, Link, useStaticQuery } from "gatsby"
+import _ from "lodash"
 import React from "react"
 
-import { Stack, Typography } from "@mui/material"
+import { Settings as SettingsIcon } from "@mui/icons-material"
+import { Button, Container, IconButton, Stack, Typography } from "@mui/material"
 
 import { CommonLayout } from "../components"
+import PageLocation from "../json/PageLocation.json"
+
+const codeStyle = {
+  fontFamily: '"Fira Code", Consolas, monospace',
+}
 
 // TODO: Finish this!
 const App = (): React.ReactElement => {
+  const data = useStaticQuery(graphql`
+    query {
+      package {
+        version
+      }
+    }
+  `)
+
+  const gamePage = _.filter(PageLocation, (page) => page.name === "Game")[0]
+  const settingsPage = _.filter(
+    PageLocation,
+    (page) => page.name === "Settings"
+  )[0]
+
   return (
     <Stack
       direction="column"
       justifyContent="center"
       alignItems="center"
-      spacing={1}
+      spacing={16}
       sx={{
         flex: "1 1 auto",
+        padding: "16px",
       }}
     >
-      <Typography variant="h6">Under construction.</Typography>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={1}
+      >
+        <Typography variant="h2">Periotris.js</Typography>
+        <Typography variant="body1" {...codeStyle}>
+          v{data.package.version}
+        </Typography>
+      </Stack>
+      <Container maxWidth="sm">
+        <Stack
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
+        >
+          <Button
+            variant="outlined"
+            size="large"
+            sx={{
+              borderColor: "white",
+              color: "white",
+              alignSelf: "stretch",
+            }}
+            component={Link}
+            to={gamePage.path}
+          >
+            Start
+          </Button>
+        </Stack>
+      </Container>
+      <IconButton
+        aria-label="open settings"
+        component={Link}
+        to={settingsPage.path}
+      >
+        <SettingsIcon />
+      </IconButton>
     </Stack>
   )
 }
