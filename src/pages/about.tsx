1683a0700b8dfd4f992edadfac283860b9097190
Rong Mantle Bao <baorong2005@126.com>
2022-08-01 18:04:47 +0800

    docs: add license header to source files

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2021-present Rong "Mantle" Bao
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see https://www.gnu.org/licenses/ .
+ */
+
 import "@fontsource/fira-code/400.css"
 
 import { graphql, useStaticQuery } from "gatsby"
@@ -80,7 +97,7 @@ const App = (): React.ReactElement => {
         <Divider variant="middle" />
         <Typography variant="h4">License</Typography>
         <Typography variant="body1" paragraph>
-          Copyright (C) 2021 Rong &quot;Mantle&quot; Bao
+          Copyright (C) 2021-present Rong &quot;Mantle&quot; Bao
         </Typography>
         <Typography variant="body1" paragraph>
           This program is free software: you can redistribute it and/or modify
